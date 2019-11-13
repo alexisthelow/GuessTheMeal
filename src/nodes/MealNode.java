2a0d8df4fb3c44606c66405d28f115dab6e4f2b4
Alexis Low <alexisthelow@gmail.com>
2019-11-13 16:18:48 -0700

    fleshed out nodes

--- src/nodes/MealNode.java
+++ src/nodes/MealNode.java
@@ -4,6 +4,22 @@ public class MealNode extends Node {
     
     // this class is only ever gonna be a leaf
     
-    private String meal;
+    private String mealName;
+    
+    public MealNode() {
+        super();
+    }
+    
+    public MealNode(String meal) {
+        this.mealName = meal;
+    }
 
+    public String getMealName() {
+        return mealName;
+    }
+
+    public void setMealName(String mealName) {
+        this.mealName = mealName;
+    }
+    
 }
