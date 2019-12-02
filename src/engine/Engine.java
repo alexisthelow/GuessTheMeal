cbca70500747f5cd4622f91a5ccb7d906581431d
Alexis Low <alexisthelow@gmail.com>
2019-12-02 13:18:29 -0700

    completed game loop

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,6 +1,8 @@
 package engine;
 
+import nodes.MealNode;
 import nodes.Node;
+import nodes.QuestionNode;
 import tree.MealTree;
 
 public class Engine {
@@ -8,9 +10,11 @@ public class Engine {
     private boolean playAgain = false;
     private boolean gameOver = false;
     private MealTree tree;
+    private ScannerResponseGetter srg;
 
     public static void main(String[] args) {
-        //TODO should just call mainLoop()
+        Engine e = new Engine();
+        e.mainLoop();
     }
     
     public void mainLoop() {
@@ -18,38 +22,78 @@ public class Engine {
         // for now just load default tree
         tree = new MealTree(); 
         tree.buildDefault();
-        Node currentNode = tree.getRoot(); // holds node we're currently working on
+        
+        // initialize ScannerResponseGetter
+        srg = new ScannerResponseGetter();
+        
+        Node currentNode; // holds node we're currently working on; at start, 
         
         do { // run through once, then ask if player wants to play again
-            // LOOP while !gameOver
+            gameOver = false;
+            currentNode = tree.getRoot(); // work starts from the root
+            
+            while (!gameOver) {// loop while !gameOver
+                
                 // process current node--question or meal?
-                    // if a question:
-                        // get response
-                        // follow correct path for response
-                        // set currentNode to node found with that path
-                        // restart loop
-                    // if a meal
-                        // set gameOver = true;
-                        // ask if player is thinking of indicated meal
-                            // if yes:
-                                // exit loop
-                            // if no:
-                                // String newMeal = what player was thinking of
-                                // String newQuestion = response to "give me a question that will distinguish [meal comp guessed] from [meal player thought of]"
-                                // boolean yesOrNo = response to "what is the correct answer for [meal comp guessed]? (true if yes, false if no)"
-                                // call tree.addMeal(currentNode, newMeal, newQuestion, yesOrNo)
-                                // exit loop
-            // end LOOP
-            // ask if player wants to play again 
-            // set playAgain to response
+                if (currentNode instanceof QuestionNode) { // it's a question
+                    
+                    // cast currentNode to a QuestionNode
+                    QuestionNode q = (QuestionNode) currentNode; 
+                    
+                    // ask the question
+                    System.out.println(q.getQuestion());
+                    
+                    // get response
+                    boolean response = srg.requestConfirmation("Please enter \"yes/y\" or \"no/n\": ");
+                    
+                    // follow correct path for response, set currentNode to that node
+                    currentNode = response ? q.getYes() : q.getNo(); // i flippin love ternary operators, yo
+                    
+                    // allow loop to restart
+                } else { // it's a meal
+                    
+                    // cast currentNode to a MealNode
+                    MealNode m = (MealNode) currentNode;
+                    
+                    // set gameOver = true (if we reach a meal node, the game is over)
+                    gameOver = true;
+                    
+                    // ask if player is thinking of indicated meal
+                    boolean response = srg.requestConfirmation("Were you thinking of " + m.getMealName() + "?: ");
+                    
+                    if (!response) { // if we guessed incorrectly, we gotta do a bunch of stuff now
+                        
+                        // ask what player was thinking of
+                        String newMeal = srg.stringRequest("What meal were you thinking of?: ");
+                        
+                        // prompt for differentiation question
+                        String newQuestion = srg.stringRequest("What is a question that distinguishes " + 
+                                m.getMealName() + " and " + newMeal + "?: ");
+                        
+                        // ask "what is the correct answer for [meal comp guessed]? (true if yes, false if no)"
+                        boolean yesOrNo = srg.requestConfirmation("What is the correct answer to that question for " +
+                                m.getMealName() + "?: ");
+                        
+                        // call tree.addMeal(currentNode, newMeal, newQuestion, yesOrNo)
+                        tree.addMeal(m, newMeal, newQuestion, yesOrNo);
+                    }
+                    
+                    // if we guessed correctly, we just allow the loop to finish 
+
+                } // end node processing
+                
+            } // end game loop
             
-            
-            
+            // ask if player wants to play again, set playAgain to response
+            playAgain = srg.requestConfirmation("Do you want to play again?: ");
             
         } while (playAgain);
         // if we get here we're not playing again
         // TODO when serialization is added, announce that the game is being saved
+        // close scanner, because it's important to be tidy
+        srg.close();
         // say goodbye
+        System.out.println("Thanks for playing! Goodbye!");
         
     }
 
